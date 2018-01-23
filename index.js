a9c4f7e46f59d297a89981bb13f6d77909534a1a
Oscar Zambrano <oscarz.developer@gmail.com>
2018-01-23 17:36:55 -0400

    Creating media component and adding styles

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app');
+
+render(<Media />, app);
+// render(que va renderizar, donde lo va a renderizar);
