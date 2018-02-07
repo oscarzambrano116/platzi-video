3967b1af933ce0da9692d4dfcf8a6cdba7345565
Oscar Zambrano <oscarz.developer@gmail.com>
2018-02-07 19:32:27 -0400

    Adding video player

---
+++ src/player/components/video-player-layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './video-player-layout.css';
+
+const VideoPlayerLayout = (props) => (
+  <div className="VideoPlayerLayout">
+    {props.children}
+  </div>
+);
+
+export default VideoPlayerLayout;
