1652618c3c6ebd4308fc3acef9e02031a118b6bc
Oscar Zambrano <oscarz.developer@gmail.com>
2018-02-17 15:06:05 -0400

    Applying logger middleware

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Map as map } from 'immutable';
 import Home from '../pages/containers/home';
 import reducer from '../reducers/index';
 import data from '../schemas/index';
 
+// JS
+// function logger({getState, dispatch}) {
+//   return (next) => {
+//     return (action) => {
+//       console.log('Estado anterior...', getState().toJS());
+//       console.log('Accion enviada', action);
+//       const value = next(action);
+//       console.log('Nuevo estado...', getState().toJS());
+//       return value;
+//     }
+//   }
+// }
+
+// ES6
+const logger = ({ getState, dispatch }) => next => action => {
+  console.log('Estado anterior...', getState().toJS());
+  console.log('Accion enviada', action);
+  const value = next(action);
+  console.log('Nuevo estado...', getState().toJS());
+  return value;
+}
+
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 const home = document.getElementById('home-container');
